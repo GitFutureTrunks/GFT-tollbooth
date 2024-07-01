3b2a36375d09d2ea42809d716adb4777a42edc14
GitFutureTrunks <145227152+GitFutureTrunks@users.noreply.github.com>
2024-07-01 16:55:21 -0500

    Tolls for Liberty

--- config.lua
+++ config.lua
@@ -1,7 +1,17 @@
 Config = {}
 
 Config.TollBooths = {
-    {location = vector4(0.0, 0.0, 0.0, 0.0), radius = 10.0, fee = 50},
-    {location = vector4(0.0, 0.0, 0.0, 0.0), radius = 10.0, fee = 50},
+    -- {location = vector4(0.0, 0.0, 0.0, 0.0), radius = 10.0, fee = 50},
+    -- {location = vector4(0.0, 0.0, 0.0, 0.0), radius = 10.0, fee = 50},
     -- add more tolls as needed playa!
+
+    -- If you have Liberty map uncomment these coords below
+    -- Dukes Bridge
+    -- {location = vector4(1605.39, 1497.4, 13.18, 324.19), radius = 3.0, fee = 50},
+    -- {location = vector4(1595.58, 1496.28, 13.17, 140.12), radius = 3.0, fee = 50},
+    -- East Bourogh Bridge
+    -- {location = vector4(801.7, 1257.34, 20.49, 359.85), radius = 5.0, fee = 50},
+    -- {location = vector4(793.8, 1257.64, 20.49, 0.97), radius = 5.0, fee = 50}, 
+    -- {location = vector4(782.76, 1246.9, 20.49, 179.44), radius = 5.0, fee = 50},
+    -- {location = vector4(774.19, 1248.16, 20.49, 179.66), radius = 5.0, fee = 50},
 }
